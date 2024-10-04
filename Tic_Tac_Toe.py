ee60d9dba536f1b6502d6bd0d135a3218a12e678
dheeraj03092006 <somadheeraj2006@gmail.com>
2024-10-04 06:25:42 +0530

    updated now

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -11,11 +11,13 @@ gameRunning = True
 #printing game board
 
 def printBoard(board):
-    print(board[0] + " | " + board[1] + " | "+ board[2])
-    print("----------")
-    print(board[3] + " | " + board[4] + " | "+ board[5])
-    print("----------")
-    print(board[6] + " | " + board[7] + " | "+ board[8])
+    print("-------------")
+    print("| " + board[0] + " | " + board[1] + " | "+ board[2] + " |")
+    print("-------------")
+    print("| " + board[3] + " | " + board[4] + " | "+ board[5] + " |")
+    print("-------------")
+    print("| " + board[6] + " | " + board[7] + " | "+ board[8] + " |")
+    print("-------------")
 
 #take player input
 
